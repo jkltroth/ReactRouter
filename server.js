5f83a8e10ffc23d0d79efec7767a0ce7662c8fb1
James Roth <jkltroth@gmail.com>
2018-02-15 19:25:30 -0500

    Deploying latest to Heroku

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 // Serve up static assets
 app.use(express.static("client/build"));
@@ -15,15 +17,20 @@ app.use(routes);
 
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
-  {
-    useMongoClient: true
-  }
-);
 
+if (process.env.MONGODB_URI) {
+
+  mongoose.connect(process.env.MONGODB_URI);
+
+} else {
+  // Connect to the Mongo DB
+  mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist", {
+      useMongoClient: true
+    }
+  );
+}
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
